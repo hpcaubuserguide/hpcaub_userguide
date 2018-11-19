1f3fe8a1a74950293d2bdcd041dcd1c38192d205
Mher Kazandjian <mherkazandjian@gmail.com>
2018-11-19 16:36:28 +0100

    add the keras example and the job script

---
+++ userdoc/tensorflow_keras_sample_job.sh
@@ -0,0 +1,14 @@
+#BSUB -J my_keras_job
+#BSUB -n 1
+#BSUB -oo my_keras_job.o%J
+#BSUB -eo my_keras_job.e%J
+#BSUB -a gpushared
+#BSUB -m node01
+
+module load singularity/2.4
+
+echo "runing on node " $HOSTNAME
+
+singularity exec \
+   /gpfs1/apps/sw/singularity/containers/deep_learning/tensorflow-1.12-devel-gpu-py3 \
+   python conv_lstm.py
