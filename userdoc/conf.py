c001c8d48658e6e0710bd13c3f5e890a9354e175
Mher Kazandjian <mherkazandjian@gmail.com>
2019-09-16 03:31:21 +0200

    use the bootstrap theme for the user guide

--- userdoc/conf.py
+++ userdoc/conf.py
@@ -21,6 +21,7 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+import sphinx_bootstrap_theme
 
 # -- General configuration ------------------------------------------------
 
@@ -52,7 +53,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'hpc user guide'
-copyright = '2018, HPC admin team, IT helpdesk'
+copyright = '2019, HPC admin team, IT helpdesk'
 author = 'HPC admin team'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -88,7 +89,9 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+# html_theme = 'alabaster'
+html_theme = 'bootstrap'
+html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
