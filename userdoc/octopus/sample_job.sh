942131d76b15967f0999c6689256e2f7f6ac1969
Mher Kazandjian <mherkazandjian@gmail.com>
2020-01-24 16:24:59 +0100

    fix typo in the argument of --account

--- userdoc/octopus/sample_job.sh
+++ userdoc/octopus/sample_job.sh
@@ -2,7 +2,7 @@
 
 ## specify the job and project name
 #SBATCH --job-name=my_job_name
-#SBATCH --account=oo_project
+#SBATCH --account=foo_project
 
 ## specify the required resources
 #SBATCH --partition=normal
